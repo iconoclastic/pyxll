d43c14af59114844d33bf8e2994a289a452f1114
christopheramirian <cheristopher.amirian@gmail.com>
2019-12-14 15:17:28 +0400

    Featured Section Version 01

---
+++ front-page.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="archive-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-featured" id="featured">
+				<?php
+					$args = array(
+						'posts_per_page'	=> 2,
+						'post_type'		=> 'post',
+						'meta_key'		=> 'is_featured',
+						'meta_value'	=> 1
+					);
+					$the_query = new WP_Query( $args );
+				?>
+
+				<?php if ( $the_query->have_posts() ) : ?>
+
+					<?php /* Start the Loop */ ?>
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+						<?php
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'loop-templates/excerpt', 'featured' );
+						?>
+
+					<?php endwhile; ?>
+
+					<?php wp_reset_query(); ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+
+				<?php endif; ?>
+
+			</main><!-- #main -->
+
+			<!-- The pagination component -->
+			<?php //understrap_pagination(); ?>
+
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div> <!-- .row -->
+
+	</div><!-- #content -->
+
+	</div><!-- #archive-wrapper -->
+
+<?php get_footer();

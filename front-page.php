2dd3350b3fb9b4842d921db4e6807d3c00362dad
christopheramirian <cheristopher.amirian@gmail.com>
2019-12-24 18:25:33 +0400

    Pagination

--- front-page.php
+++ front-page.php
@@ -24,89 +24,93 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-			<main class="site-featured" id="featured">
-				<?php
-					$args = array(
-						'posts_per_page'	=> 2,
-						'post_type'		=> 'post',
-						'meta_key'		=> 'is_featured',
-						'meta_value'	=> 1
-					);
-					$the_query = new WP_Query( $args );
-				?>
+			<?php if (get_query_var('paged') == 0) : ?>
+				<main class="site-featured" id="featured">
+					<?php
+						$args = array(
+							'posts_per_page'	=> 2,
+							'post_type'		=> 'post',
+							'meta_key'		=> 'is_featured',
+							'meta_value'	=> 1
+						);
+						$the_query = new WP_Query( $args );
+					?>
 
-				<?php if ( $the_query->have_posts() ) : ?>
+					<?php if ( $the_query->have_posts() ) : ?>
 
-					<small class="type-headline">Featured</small>
-					<?php /* Start the Loop */ ?>
-					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						<small class="type-headline">Featured</small>
+						<?php /* Start the Loop */ ?>
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-						<?php
+							<?php
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/excerpt', 'featured' );
-						?>
+							/*
+							* Include the Post-Format-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							*/
+							get_template_part( 'loop-templates/excerpt', 'featured' );
+							?>
 
-					<?php endwhile; ?>
+						<?php endwhile; ?>
 
-					<?php wp_reset_query(); ?>
+						<?php wp_reset_query(); ?>
 
-				<?php else : ?>
+					<?php else : ?>
 
-					<?php //get_template_part( 'loop-templates/content', 'none' ); ?>
+						<?php //get_template_part( 'loop-templates/content', 'none' ); ?>
 
-				<?php endif; ?>
+					<?php endif; ?>
 
-			</main><!-- #featured -->
+				</main><!-- #featured -->
 
-			<main class="site-trending" id="trending">
-				<?php
-					$args = array(
-						'posts_per_page'	=> 2,
-						'post_type'		=> 'post',
-						'meta_key'		=> 'is_trending',
-						'meta_value'	=> 1
-					);
-					$the_query = new WP_Query( $args );
-				?>
+				<main class="site-trending" id="trending">
+					<?php
+						$args = array(
+							'posts_per_page'	=> 2,
+							'post_type'		=> 'post',
+							'meta_key'		=> 'is_trending',
+							'meta_value'	=> 1
+						);
+						$the_query = new WP_Query( $args );
+					?>
 
-				<?php if ( $the_query->have_posts() ) : ?>
+					<?php if ( $the_query->have_posts() ) : ?>
 
-					<small class="type-headline">Trending</small>
-					<?php /* Start the Loop */ ?>
-					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						<small class="type-headline">Trending</small>
+						<?php /* Start the Loop */ ?>
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-						<?php
+							<?php
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/excerpt', 'featured' );
-						?>
+							/*
+							* Include the Post-Format-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							*/
+							get_template_part( 'loop-templates/excerpt', 'featured' );
+							?>
 
-					<?php endwhile; ?>
+						<?php endwhile; ?>
 
-					<?php wp_reset_query(); ?>
+						<?php wp_reset_query(); ?>
 
-				<?php else : ?>
+					<?php else : ?>
 
-					<?php //get_template_part( 'loop-templates/content', 'none' ); ?>
+						<?php //get_template_part( 'loop-templates/content', 'none' ); ?>
 
-				<?php endif; ?>
+					<?php endif; ?>
 
-			</main><!-- #trending -->
+				</main><!-- #trending -->
+			<?php endif; ?>
 
 			<main class="site-main" id="main">
 				<?php
-					$args = array(
+					$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+					$args_posts = array(
 						'post_type'			=> 'post',
 						'posts_pre_page'	=> -1,
+						'paged'				=> $paged,
 						'meta_query'		=> array(
 							'relation' 			=> 'AND',
 							array(
@@ -121,14 +125,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 							)
 						),
 					);
-					$the_query = new WP_Query( $args );
+					$the_query_posts = new WP_Query( $args_posts );
+
 				?>
 
-				<?php if ( $the_query->have_posts() ) : ?>
+				<?php if ( $the_query_posts->have_posts() ) : ?>
 
 					<small class="type-headline">Latest</small>
 					<?php /* Start the Loop */ ?>
-					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+					<?php while ( $the_query_posts->have_posts() ) : $the_query_posts->the_post(); ?>
 
 						<?php
 
@@ -153,7 +158,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</main><!-- #main -->
 
 			<!-- The pagination component -->
-			<?php //understrap_pagination(); ?>
+			<?php understrap_pagination(); ?>
 
 			<!-- Do the right sidebar check -->
 			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
